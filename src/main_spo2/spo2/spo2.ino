4fd0bac7da735868c6412309a017b6d1e010f05a
Luis Fernando R <luis_rogu@outlook.com>
2020-11-22 14:16:48 -0600

    spo2 system working

---
+++ src/main_spo2/spo2/spo2.ino
@@ -0,0 +1,170 @@
+/*
+  Optical Heart Rate Detection (PBA Algorithm) using the MAX30105 Breakout
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 2nd, 2016
+  https://github.com/sparkfun/SparkFun_MAX3010x_Sensor_Library/tree/master/examples
+  This is a demo to show the reading of heart rate or beats per minute (BPM) using
+  a Penpheral Beat Amplitude (PBA) algorithm.
+
+  It is best to attach the sensor to your finger using a rubber band or other tightening
+  device. Humans are generally bad at applying constant pressure to a thing. When you
+  press your finger against the sensor it varies enough to cause the blood in your
+  finger to flow differently which causes the sensor readings to go wonky.
+
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed) (Wemos/ESP8266: use 3.3v -> VIN)
+  -GND = GND
+  -SDA = A4 (or SDA) (Wemos/ESP8266: D2)
+  -SCL = A5 (or SCL) (Wemos/ESP8266: D1)
+  -INT = Not connected
+
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
+*/
+
+#include "MAX30105.h"
+#include "heartRate.h"
+
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
+
+long samplesTaken = 0; //Counter for calculating the Hz or read rate
+long unblockedValue; //Average IR at power up
+long startTime; //Used to calculate measurement rate
+
+int perCent; 
+int degOffset = 0.5; //calibrated Farenheit degrees
+int irOffset = 1800;
+int count;
+int noFinger;
+//auto calibrate
+int avgIr;
+int avgTemp;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  //The LEDs are very low power and won't affect the temp reading much but
+  //you may want to turn off the LEDs to avoid any local heating
+  particleSensor.setup(0); //Configure sensor. Turn off LEDs
+  particleSensor.enableDIETEMPRDY(); //Enable the temp ready interrupt. This is required.
+  
+  //Setup to sense up to 18 inches, max LED brightness
+  byte ledBrightness = 25; //Options: 0=Off to 255=50mA=0xFF hexadecimal. 100=0x64; 50=0x32 25=0x19
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 2048; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  particleSensor.enableDIETEMPRDY(); //Enable the temp ready interrupt. This is required.
+}
+
+void loop()
+{
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(", BPM=");
+  Serial.print(beatsPerMinute);
+  Serial.print(", Avg BPM=");
+  Serial.print(beatAvg);
+
+
+  Serial.print(" ");
+  perCent = irValue / irOffset;
+  Serial.print("Oxygen=");
+  Serial.print(perCent);
+  Serial.print("%");
+  //Serial.print((float)samplesTaken / ((millis() - startTime) / 1000.0), 2);
+
+  float temperatureF = particleSensor.readTemperatureF(); //Because I am a bad global citizen
+  temperatureF = temperatureF + degOffset;
+  
+  Serial.print(" Temp(F)=");
+  Serial.print(temperatureF, 2);
+  Serial.print("°");
+
+  Serial.print(" IR=");
+  Serial.print(irValue);
+
+  if (irValue < 50000) {
+    Serial.print(" No finger?");
+    noFinger = noFinger+1;
+  } else {
+    //only count and grab the reading if there's something there.
+    count = count+1;
+    avgIr = avgIr + irValue;
+    avgTemp = avgTemp + temperatureF;
+    Serial.print(" ... ");
+  }
+
+  //Get an average IR value over 100 loops
+  if (count == 100) {
+    avgIr = avgIr / count;
+    avgTemp = avgTemp / count;
+    Serial.print(" avgO=");
+    Serial.print(avgIr);
+    Serial.print(" avgF=");
+    Serial.print(avgTemp);
+
+    Serial.print(" count=");
+    Serial.print(count);
+    //reset for the next 100
+    count = 0; 
+    avgIr = 0;
+    avgTemp = 0;
+  }
+
+  //turn off the LED if there's no finger
+  //this doesn't work yet.
+  //if (noFinger == 500) {
+  //  particleSensor.setPulseAmplitudeRed(0);
+    //delay(5000);
+  //} 
+  //Serial.print(noFinger);
+  Serial.println();
+}
